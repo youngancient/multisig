70b49dd7d3fe4ee92d0674071fb6b4e0238afb54
youngancient <tochyokoye@gmail.com>
2024-09-12 16:36:29 +0100

    added interaction images

--- scripts/interactions.ts
+++ scripts/interactions.ts
@@ -1,13 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // get token address and contrat
+  // get token address and contrat  
 
-  const clownTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const clownTokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
   const CTK = await ethers.getContractAt("IERC20", clownTokenAddress);
 
   //   get multisig factory address and contract
-  const multisigFactoryAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  const multisigFactoryAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const multisigFactory = await ethers.getContractAt(
     "MultisigFactory",
     multisigFactoryAddress
@@ -31,13 +31,14 @@ async function main() {
     addr3.address,
     addr4.address,
   ];
-//   const createMultiSigTx = await multisigFactory.createMultisigWallet(
-//     quorum,
-//     validSigners,
-//     owner.address
-//   );
-//   createMultiSigTx.wait();
+    const createMultiSigTx = await multisigFactory.createMultisigWallet(
+      quorum,
+      validSigners,
+      owner.address
+    );
+    createMultiSigTx.wait();
 
+    console.log("Clone multisig tx: ",createMultiSigTx);  // tx
   //   create multiple clones
 
   //   check the number of clones after creating
@@ -46,7 +47,7 @@ async function main() {
     "number of multisig clones after new clone creation: " + numberOfClones
   );
 
-//   console.log("transaction: ", createMultiSigTx);
+  //   console.log("transaction: ", createMultiSigTx);
 
   // get clone
   let cloneId = numberOfClones;
@@ -69,25 +70,30 @@ async function main() {
 
   //   initiate transfer
 
-//   const transferAmount = ethers.parseUnits("1000", 18);
-//   const initiateTransferTx = await MultiSig.initiateTransfer(
-//     addr2.address,
-//     transferAmount,
-//     clownTokenAddress
-//   );
-//   initiateTransferTx.wait();
+    const transferAmount = ethers.parseUnits("1000", 18);
+    const initiateTransferTx = await MultiSig.initiateTransfer(
+      addr2.address,
+      transferAmount,
+      clownTokenAddress
+    );
+    initiateTransferTx.wait();
 
-//   console.log(initiateTransferTx);
+    console.log("Initiate transfer Tx: ",initiateTransferTx);        // tx
 
-  console.log(
-    "is factory a valid signer: ",
-    await MultiSig.validSigners(multisigFactoryAddress)
-  );
-  console.log("is owner a valid signer: ", await MultiSig.validSigners(owner));
-  console.log("no of valid signers: ", await MultiSig.noOfValidSigners());
+  console.log("No of transactions: ",await MultiSig.txCounter());
 
-  // approve
+  // approve the initiated transaction
+  let id = 1;
+  const transferTrx = await MultiSig.transactions(id);
+  console.log("no of Tx approval before: ",transferTrx.noOfApprovals);
 
+
+  const approveTx = (await MultiSig.connect(addr2).approve(id)).wait();
+
+  console.log("Approve transfer Tx: ", approveTx);
+
+  console.log("no of Tx approval before: ",transferTrx.noOfApprovals);
+  
   // initiate update Quorum
 
   //approve
